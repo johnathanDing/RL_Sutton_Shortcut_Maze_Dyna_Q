f6ca54fcb67e0f217022ecf955347b94cc168ebb
Xiaoyue Ding <johnathand1102@gmail.com>
2021-10-26 14:45:39 -0700

    Finished MazeModel and Unit testing

--- Shortcut_Maze_Dyna_Q/MazeModel.hpp
+++ Shortcut_Maze_Dyna_Q/MazeModel.hpp
@@ -67,6 +67,10 @@ public:
     
     /// Get a random past experience from the model, according to Dyna-Q+
     MazePastExp getPastResponse_DynaQ_Plus() const;
+    
+    /// Reacquires the Dyna-Q+ model after a MazeEnv change
+    /// @param curr_state State to be updated in model.
+    void reacquireModel_DynaQ_Plus(std::tuple<int, int> curr_state);
 };
 
 #endif /* MazeModel_hpp */
