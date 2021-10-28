fc9051328d639f203726dbfd5cf2a9cfe8c9d66e
Xiaoyue Ding <johnathand1102@gmail.com>
2021-10-27 21:07:52 -0700

    Writing Dyna training main

--- Shortcut_Maze_Dyna_Q/main.cpp
+++ Shortcut_Maze_Dyna_Q/main.cpp
@@ -32,7 +32,7 @@ int main() {
     MazeVisualizer maze_visual(grid_world);
     
     // Prepare to record training data in a file
-    int time_step(0), cum_reward_DynaQ(0), cum_reward_DynaQ_Plus(0);
+    int time_step(0), time_limit(6000), cum_reward_DynaQ(0), cum_reward_DynaQ_Plus(0);
     std::ofstream training_file("Training_statistics.txt");
     if (!training_file) {
         // If somehow the file is not opened
@@ -47,4 +47,26 @@ int main() {
     
     // Visualize the maze first
     maze_visual.drawMaze();
+    
+    // Print a message
+    std::cout << "Training has started!" << "\n";
+    
+    // Initialize state-action variables
+    std::tuple<int, int> curr_state_DynaQ, curr_move_DynaQ, curr_state_DynaQ_Plus, curr_move_DynaQ_Plus;
+    // Initialize the response structs to be goal state, for easy transition at the start of training
+    MazeResponse maze_response_DynaQ{{grid_world.getGoalPos()}, {1}, {true}},
+                 maze_response_DynaQ_Plus{{grid_world.getGoalPos()}, {1}, {true}};
+    
+    while (time_step < time_limit) {
+        // If a Dyna-Q episode is just finished, start a new one
+        if (maze_response_DynaQ.finished) {
+            curr_state_DynaQ = grid_world.getStartPos();
+            curr_move_DynaQ = maze_policy.getSoftPolicy_DynaQ(curr_state_DynaQ);
+        }
+        // If a Dyna-Q+ episode is just finished, start a new one
+        if (maze_response_DynaQ_Plus.finished) {
+            curr_state_DynaQ_Plus = grid_world.getStartPos();
+            curr_move_DynaQ_Plus = maze_policy.getSoftPolicy_DynaQ_Plus(curr_state_DynaQ_Plus, time_step);
+        }
+    }
 }
